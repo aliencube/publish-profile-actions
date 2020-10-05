abac0de7f11a48f53b0cb0fa80e8ff2308e2b471
Justin Yoo <justin.yoojh@gmail.com>
2020-10-06 08:32:06 +0900

    Refactor to PowerShell (#3)

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM mcr.microsoft.com/azure-cli:latest
+FROM mcr.microsoft.com/azure-powershell:latest
 
 LABEL "com.github.actions.name"="Azure App Service Publish Profile"
 LABEL "com.github.actions.description"="Retrieve the publish profile of Azure Web App or Functions App in XML format"
@@ -9,12 +9,7 @@ LABEL "repository"="http://github.com/aliencube/publish-profile-actions"
 LABEL "homepage"="http://github.com/aliencube"
 LABEL "maintainer"="Justin Yoo <no-reply@aliencube.com>"
 
-# Install jq
-RUN apk update && apk add \
-    jq \
- && rm -rf /var/cache/apk/*
+ADD entrypoint.ps1 /entrypoint.ps1
+RUN chmod +x /entrypoint.ps1
 
-ADD entrypoint.sh /entrypoint.sh
-RUN chmod +x /entrypoint.sh
-
-ENTRYPOINT ["/entrypoint.sh"]
+ENTRYPOINT ["pwsh", "-File", "/entrypoint.ps1"]
---
+++ entrypoint.ps1
@@ -0,0 +1,32 @@
+Param(
+    [string]
+    [Parameter(Mandatory=$true)]
+    $ResourceGroupName,
+
+    [string]
+    [Parameter(Mandatory=$true)]
+    $AppName
+)
+
+$clientId = ($env:AZURE_CREDENTIALS | ConvertFrom-Json).clientId
+$clientSecret = ($env:AZURE_CREDENTIALS | ConvertFrom-Json).clientSecret | ConvertTo-SecureString -AsPlainText -Force
+$tenantId = ($env:AZURE_CREDENTIALS | ConvertFrom-Json).tenantId
+
+$credentials = New-Object System.Management.Automation.PSCredential($clientId, $clientSecret)
+
+$connected = Connect-AzAccount -ServicePrincipal -Credential $credentials -Tenant $tenantId
+
+$profile = Get-AzWebAppPublishingProfile `
+    -ResourceGroupName $ResourceGroupName `
+    -Name $AppName
+
+$profile = $profile.Replace("`r", "").Replace("`n", "")
+
+Write-Output "::set-output name=profile::$profile"
+
+Remove-Variable profile
+Remove-Variable connected
+Remove-Variable credentials
+Remove-Variable tenantId
+Remove-Variable clientSecret
+Remove-Variable clientId
